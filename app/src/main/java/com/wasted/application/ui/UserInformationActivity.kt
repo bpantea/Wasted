2fa16bdea32d9da603e5d132f74bcf2da097e416
Rafa <alexandru.rafaila@yahoo.com>
2020-01-12 22:36:58 +0200

    UI for user information

--- app/src/main/java/com/wasted/application/ui/UserInformationActivity.kt
+++ app/src/main/java/com/wasted/application/ui/UserInformationActivity.kt
@@ -4,10 +4,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.wasted.application.R
 
-class userIndormation : AppCompatActivity() {
+class UserInformationActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_user_indormation)
+        setContentView(R.layout.activity_user_information)
     }
 }
