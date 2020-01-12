1e69816bf2fd5a886a9462e5e8c6e60780af60f0
Rafa <alexandru.rafaila@yahoo.com>
2020-01-12 21:40:17 +0200

    UI for user information

--- app/src/main/java/com/wasted/application/ui/MainActivity.kt
+++ app/src/main/java/com/wasted/application/ui/MainActivity.kt
@@ -5,18 +5,19 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import com.wasted.application.R
+import com.wasted.application.ui.auth.LoginActivity
 import com.wasted.application.ui.auth.LoginFacebookActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_user_indormation)
 
     }
 
     fun openLogin(view: View) {
-        val intent = Intent(this, LoginFacebookActivity::class.java)
+       val intent = Intent(this, LoginActivity::class.java)
         startActivity(intent)
     }
 }
---
+++ app/src/main/java/com/wasted/application/ui/userIndormation.kt
@@ -0,0 +1,13 @@
+package com.wasted.application.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.wasted.application.R
+
+class userIndormation : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_indormation)
+    }
+}
