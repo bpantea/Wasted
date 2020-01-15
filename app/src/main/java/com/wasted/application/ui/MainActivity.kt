176f98d978a96628ebd4283e8f6e599484220eb0
Deni <pirvu.denisa01@gmail.com>
2020-01-15 14:02:43 +0200

    done

--- app/src/main/java/com/wasted/application/ui/MainActivity.kt
+++ app/src/main/java/com/wasted/application/ui/MainActivity.kt
@@ -7,6 +7,7 @@ import android.util.Log
 import android.view.View
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat.startActivity
 import androidx.viewpager.widget.ViewPager
 
 import com.wasted.application.R
@@ -15,15 +16,19 @@ import com.wasted.application.ui.auth.LoginFacebookActivity
 class MainActivity : AppCompatActivity() {
 
 
-    fun openLogin(view: View) {
-        val intent = Intent(this, LoginFacebookActivity::class.java)
-        startActivity(intent)
-    }
 
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_stats_layout)
+        val intent = Intent(this, ActivityStats::class.java)
+        startActivity(intent)
         Log.d(TAG, "onCreate: Started...")
+
+       /*val btnSecondActivity = findViewById<Button>(R.id.buttonSecondActivity)
+        btnSecondActivity.setOnClickListener{
+            val intent = Intent(this, ActivityStats::class.java)
+            startActivity(intent)
+        }*/
 
     }
 
@@ -31,18 +36,15 @@ class MainActivity : AppCompatActivity() {
         val intent = Intent(this, LoginFacebookActivity::class.java)
         startActivity(intent)
     }
-}
-        val btnSecondActivity = findViewById<Button>(R.id.buttonSecondActivity)
-        btnSecondActivity.setOnClickListener{
-            val intent = Intent(this, ActivityStats::class.java)
-            startActivity(intent)
-        }
-    }
+
+
+
+
 
 
 
     companion object {
 
-        private val TAG = "MainActivity"
+        private val TAG = "ActivityStats"
     }
 }
