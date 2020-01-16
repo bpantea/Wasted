2b5154f96b6b4b3e889ecb44ab275edfadfebde2
Beniamin Pantea <beniamin.pantea@catalysts.cc>
2020-01-16 22:03:07 +0200

    added endpoint in backend for login

--- app/src/main/java/com/wasted/application/backend/AuthService.kt
+++ app/src/main/java/com/wasted/application/backend/AuthService.kt
@@ -2,40 +2,34 @@ package com.wasted.application.backend
 
 import androidx.lifecycle.MutableLiveData
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.wasted.application.model.GoogleUserDto
 import com.wasted.application.model.User
 import retrofit2.Call
-import retrofit2.http.Field
-import retrofit2.http.FormUrlEncoded
-import retrofit2.http.GET
-import retrofit2.http.POST
+import retrofit2.http.*
 
-data class HelloWorld(val response: String) {
+data class StringResponse(val response: String) {
     override fun toString(): String {
         return response
     }
 }
 
-data class AuthByToken(val Authorization: String)
-
 interface AuthService {
 
     companion object {
-        val currentUser: MutableLiveData<User?> = MutableLiveData()
 
-        fun updateCurrentUser(account: GoogleSignInAccount?) {
-            if (account == null) {
-                currentUser.value = null
-            } else {
-                val user = User(account.id!!, account.displayName!!, account.email, account.photoUrl?.toString(), null, null, null)
-                currentUser.value = user
-            }
+        val fullUser: MutableLiveData<User?> = MutableLiveData()
+
+        val authenticationGoogleUser: MutableLiveData<GoogleSignInAccount?> = MutableLiveData()
+
+        fun updateAuthenticationUser(account: GoogleSignInAccount?) {
+            authenticationGoogleUser.value = account
+        }
+
+        fun updateFullUser(user: User?) {
+            fullUser.value = user
         }
     }
 
-    @POST("oauth2/authorization/token")
-    @FormUrlEncoded
-    fun send(@Field("Authorization") idToken: String): Call<Any>
-
-    @GET("api/user/unauthenticated")
-    fun helloWorld(): Call<HelloWorld>
+    @POST("api/user/google-user")
+    fun login(@Body googleUser: GoogleUserDto): Call<User>
 }
---
+++ app/src/main/java/com/wasted/application/model/GoogleUserDto.kt
@@ -0,0 +1,6 @@
+package com.wasted.application.model
+
+data class GoogleUserDto(var id: String,
+                         var displayName: String,
+                         var email: String?,
+                         var profilePicture: String?)
